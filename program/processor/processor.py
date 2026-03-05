9723bf35b7e234e96bd188c9f62aadd50b9dd11c
Careroms <careroms.wasu.jp@gmail.com>
2026-03-05 14:18:30 +0700

    add default processor in main

---
+++ program/processor/processor.py
@@ -0,0 +1,11 @@
+import yaml, sys
+
+from pathlib import Path
+from pydantic import ValidationError
+
+class Processor:
+  def __init__(self, config_path: str):
+    pass
+
+  def execute(self):
+    pass
